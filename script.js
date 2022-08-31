e7a6b9e0e705b9faea5c57ff41095446dbdf7b5a
chris <chrislanderos05@gmail.com>
2022-08-31 15:15:57 -0700

    i tried to finish it myself and tried for 1 and half im confused on why it wont work

---
+++ script.js
@@ -0,0 +1,57 @@
+const quotes = [
+  {
+    quote: "The Hardest Choices Require The Strongest Wills.",
+    source: "Thanos",
+    citation: "Avengers-infinity",
+    year: "2018",
+    tags: "thanos",
+    timeline: "2017-2018",
+  },
+
+  {
+    quote:
+      "The death of one man is a tragedy. The death of millions is a statistic. ",
+    source: "Joseph Stalin",
+  },
+
+  {
+    quote:
+      "Your world as you knew it is gone. How far would you go to bring it back?",
+    source: "Captian Price",
+  },
+
+  {
+    quote: "The Hardest Choices Require The Strongest Wills.",
+    source: "Thanos",
+  },
+];
+function getRandomQuote() {
+  let range = quotes.length;
+  console.log(range);
+
+  let randomNum = Math.random(Math.random() * range);
+  console.log(randomNum);
+
+  let getRandomQuote = quotes[randomNum];
+  return RandomQuote;
+}
+
+function printQuote() {
+  let printedQuote = getRandomQuote();
+  let quoteProperty = printedQuote.quote;
+  let sourceProperty = printedQuote.source;
+  let citationProperty = printedQuote.citation;
+  let yearProperty = printedQuote.year;
+
+  if (printedQuote.hasOwnProperty("citation")) {
+    htmlString += <span class="citation"> ${citationPrerty}</span>;
+  }
+  if (printedQuote.hasOwnProperty("year")) {
+    htmlString += <span class="year"> ${yearProperty}</span>;
+  }
+}
+
+printQuote();
+
+document.getElementById("load-quote").addEventListener("click", printQuote);
+op;
